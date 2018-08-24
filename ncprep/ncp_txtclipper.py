c00eca087be83f332bec97f4b9a942b7094350e1
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-24 17:41:37 +0300

    updated legecy argument parser for clipper

--- ncprep/ncp_txtclipper.py
+++ ncprep/ncp_txtclipper.py
@@ -4,18 +4,13 @@
 from __future__ import print_function
 
 # Import python libraries
-import os
 import sys
-import argparse
 import pandas as pd
-try:
-    from pyrainbowterm import *
-except ImportError:
-    print('Can not import pyrainbowterm!', log_type='error')
-    print('Try: pip install pyrainbowterm', log_type='hint')
-    sys.exit(1)
+from pyrainbowterm import *
+
 # Import file_operations
 import _operations
+
 
 # Source code meta data
 __author__ = 'Dalwar Hossain'
@@ -23,7 +18,7 @@ __email__ = 'dalwar.hossain@protonmail.com'
 
 
 # Clip data frame
-def clip_data_frame(data_frame=None, start_date=None, periods=None):
+def __clip_data_frame(data_frame=None, start_date=None, periods=None):
     """
     This function clips (slices) pandas data frame according to dates and interval
     :param data_frame: Python pandas data frame
@@ -59,7 +54,7 @@ def clip_data_frame(data_frame=None, start_date=None, periods=None):
 
 
 # Load input file
-def load_file(input_file=None, delimiter=None):
+def __load_file(input_file=None, delimiter=None):
     """
     This function loads the input file into a python pandas data frame
     :param input_file: Input file path
@@ -117,10 +112,10 @@ def clip_text(input_file=None, delimiter=None, start_date=None, interval=None):
     # If sanity check is passed, read and clip the text
     if sanity_status == 1:
         # Load input file
-        data_frame = load_file(input_file=input_file, delimiter=delimiter)
+        data_frame = __load_file(input_file=input_file, delimiter=delimiter)
 
         # Clip data frame
-        clipped_text = clip_data_frame(data_frame=data_frame, start_date=start_date, periods=interval)
+        clipped_text = __clip_data_frame(data_frame=data_frame, start_date=start_date, periods=interval)
 
         # Create output file of the clipped data
         file_name, ext = input_file.rsplit('.', 1)
@@ -129,61 +124,3 @@ def clip_text(input_file=None, delimiter=None, start_date=None, interval=None):
     else:
         print('Sanity check failed!', log_type='error', color='red')
         sys.exit(1)
-
-
-# Create command center
-def command_center(input_file=None, delimiter=None, start_date=None, interval=None):
-    """
-    This function controls the other functions
-    :param input_file: Input file to clip
-    :param delimiter: Column separator for input file
-    :param start_date: Start date of clipping (dd-mm-YYYY)
-    :param interval: for how many days (int)
-    :return: <>
-    """
-    print('Initializing.....', color='green', log_type='info')
-    # Clip text according to input
-    clip_text(input_file, delimiter, start_date, interval)
-    pass
-
-
-# Standard boilerplate for running this source code file as a standalone segment
-if __name__ == '__main__':
-    """
-    Parse arguments and follow through to mission control
-    """
-    # Print initial message
-    message = 'This script uses linux timestamps. Input file format => (source target weight timestamp)'
-    _operations.initial_message(os.path.basename(__file__), custom_message=message)
-
-    # Create parser
-    parser = argparse.ArgumentParser(add_help=True)
-
-    parser.add_argument('-i', '--input-file', action='store', dest='input_file', required=True,
-                        help='Input file absolute path. E.g. /home/user/data/input/file_name.txt')
-    parser.add_argument('-d', '--delimiter', action='store', dest='delimiter', required=False,
-                        help='Separator for the input and output file. E.g. (,)/(";" need to be quoted)/tab/space. '
-                             'Default (whitespace)')
-    parser.add_argument('-s', '--from-date', action='store', dest='start_date', required=True,
-                        help='Start date for clipping the file (dd-mm-YYYY)')
-    parser.add_argument('-e', '--interval', action='store', dest='interval', required=True,
-                        help='End interval. (int) e.g. 15 days from start date')
-
-    # Parse arguments
-    args = parser.parse_args()
-
-    # Double checking the arguments
-    if args.delimiter:
-        _delimiter = args.delimiter
-    else:
-        print('No delimiter provided! Default delimiter [whitespace] will be used.....', log_type='info')
-        _delimiter = None
-    if args.start_date:
-        _start_date = args.start_date
-        if len(_start_date.split('-')[0]) == 4:
-            _start_date_ = _start_date
-        else:
-            print('Wrong date format! Try: (yyyy-mm-dd)', log_type='error', color='red')
-            sys.exit(1)
-
-    command_center(input_file=args.input_file, delimiter=_delimiter, start_date=_start_date_, interval=args.interval)
