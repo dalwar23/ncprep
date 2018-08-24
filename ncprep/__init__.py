ce60a7aa27f426776b429c1524512f24b1ddacab
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-08-24 17:40:16 +0300

    added version and doc string

--- ncprep/__init__.py
+++ ncprep/__init__.py
@@ -2,17 +2,27 @@
 # -*- coding: utf-8 -*-
 
 """
-This package is built for preparing blockchain data for community detection
-with the another python package named - neochain
+This package cna filter text based on column indexes, it can also clip data
+based on timestamp and interval and map string data to numeric data if the
+file is formatted as  -> source target weight timestamp
+This package is specially designed for using with another python package
+called 'neochain' to detect community structures in blockchain network
 
-Import this package with: import ncprep
+To install: python setup.py install
+To use: import ncprep as ncp
 """
 
-from ncp_txtfilter import filter_columns
-from ncp_txtclipper import clip_text
-from ncp_txtmapper import numeric_mapper
 
 # Source code meta data
 __author__ = 'Dalwar Hossain'
 __email__ = 'dalwar.hossain@protonmail.com'
 
+
+# Handle imports
+from ncp_txtfilter import filter_columns
+from ncp_txtclipper import clip_text
+from ncp_txtmapper import numeric_mapper
+
+
+# Version
+__version__ = 1.0
